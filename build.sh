efe37a2edb4eba267a35e98a70262c399d9f2c10
Dave Compton <davecompton7@gmail.com>
2020-11-26 20:33:31 -0800

    moved setting of GO variables

--- build.sh
+++ build.sh
@@ -1,11 +1,12 @@
 #! /bin/bash
-export GOPATH=~/go
-export GOCACHE="$(mktemp -d)"
-mkdir -p $GOPATH
-
+sudo apt install -y   make curl
 sudo apt install -y   libapparmor-dev
 sudo apt install -y libprotobuf-c-dev btrfs-progs 
 sudo apt install -y git   golang-go   go-md2man   iptables   libassuan-dev   libbtrfs-dev   libc6-dev   libdevmapper-dev   libglib2.0-dev   libgpgme-dev   libgpg-error-dev   libprotobuf-dev    libseccomp-dev   libselinux1-dev   libsystemd-dev   pkg-config   runc   uidmap
+
+export GOPATH=~/go
+export GOCACHE="$(mktemp -d)"
+mkdir -p $GOPATH
 
 sudo mkdir -p /etc/containers
 cd /etc/containers
